e5c8367d6d9de011f06212317338ff9587c03956
Surya Manikanta <suryamanikanta@Suryas-MacBook-Air.local>
2022-09-09 16:06:30 +0530

    initial commit

---
+++ src/main/java/com/odos/smartaqua/SmartaquaApplication.java
@@ -0,0 +1,13 @@
+package com.odos.smartaqua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartaquaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartaquaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odos/smartaqua/SmartaquaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odos.smartaqua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartaquaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
